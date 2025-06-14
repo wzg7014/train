f4d8b199d348bdf30c082a367c2ace15e0a5303a
wzg <wzg7014@163.com>
2025-06-14 21:32:42 +0800

    初始化

---
+++ src/test/java/com/wzg/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzg.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
