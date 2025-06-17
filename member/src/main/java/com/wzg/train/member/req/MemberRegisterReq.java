c32f27d46d10863c889f4d4a38b3b34dc77e1768
wzg <wzg7014@163.com>
2025-06-17 19:53:03 +0800

    4.19 集成校验框架validation

--- member/src/main/java/com/wzg/train/member/req/MemberRegisterReq.java
+++ member/src/main/java/com/wzg/train/member/req/MemberRegisterReq.java
@@ -1,5 +1,6 @@
 package com.wzg.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,6 +10,7 @@ import lombok.ToString;
 @ToString
 public class MemberRegisterReq {
 
+    @NotBlank(message = "【手机号】不能为空")
     private String mobile;
 
 
