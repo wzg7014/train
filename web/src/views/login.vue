adabf5888e47db61b95fe6fc3246899a9b22b828
wzg <wzg7014@163.com>
2025-06-23 08:59:12 +0800

    5.9 集成axios，发起后端请求会报跨域错误

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -41,7 +41,7 @@
 
 <script>
 import {defineComponent, reactive} from 'vue';
-
+import axios from "axios";
 
 export default defineComponent({
   name: "login-view",
@@ -50,17 +50,28 @@ export default defineComponent({
       mobile: '13000000000',
       code: '',
     });
+
     const onFinish = values => {
       console.log('Success', values);
     }
+
     const onFinishFailed = errorInfo => {
       console.log('Failed', errorInfo);
+    }
+
+    const sendCode = () => {
+      axios.post("http://localhost:8000/member/member/send-code", {
+        mobile:loginForm.mobile
+      }).then(response => {
+        console.log(response);
+      })
     }
 
     return {
       loginForm,
       onFinish,
-      onFinishFailed
+      onFinishFailed,
+      sendCode
     };
   },
 });
