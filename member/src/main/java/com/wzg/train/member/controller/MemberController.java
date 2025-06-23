ca362297e4b41a5f10d2651d6eca1b0293173ddb
wzg <wzg7014@163.com>
2025-06-23 09:21:22 +0800

    5.9 将后端接口改为body json方式，前后端联调成功

--- member/src/main/java/com/wzg/train/member/controller/MemberController.java
+++ member/src/main/java/com/wzg/train/member/controller/MemberController.java
@@ -8,10 +8,7 @@ import com.wzg.train.member.resp.MemberLoginResp;
 import com.wzg.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -38,7 +35,7 @@ public class MemberController {
     }
 
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req) {
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
     }
