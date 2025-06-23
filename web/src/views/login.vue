6dbfa02272c7fe7ecf4e20e817c896b14717e033
wzg <wzg7014@163.com>
2025-06-23 12:45:23 +0800

    5.12 登陆成功后，跳转到控制台主页

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -41,10 +41,12 @@
 import {defineComponent, reactive} from 'vue';
 import axios from "axios";
 import {notification} from "ant-design-vue";
+import {useRouter} from "vue-router";
 
 export default defineComponent({
   name: "login-view",
   setup() {
+    const router = useRouter();
     const loginForm = reactive({
       mobile: '13000000000',
       code: '',
@@ -69,6 +71,8 @@ export default defineComponent({
         let data = response.data;
         if (data.success){
           notification.success({ description:'登陆成功！'})
+          //登陆成功，跳转到控制台主页
+          router.push("/")
         }else {
           notification.error({ description: data.message});
         }
