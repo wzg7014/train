e3c91631ab0ed7f4b6107644401a94491836a368
wzg <wzg7014@163.com>
2025-06-25 19:12:43 +0800

    7.3 增加乘车人新增接口

---
+++ member/src/main/java/com/wzg/train/member/controller/PassengerController.java
@@ -0,0 +1,26 @@
+package com.wzg.train.member.controller;
+
+import com.wzg.train.common.resp.CommonResp;
+import com.wzg.train.member.req.PassengerSaveReq;
+import com.wzg.train.member.service.PassengerService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passenger")
+public class PassengerController {
+
+    @Resource
+    private PassengerService passengerService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
+        passengerService.save(req);
+        return new CommonResp<>();
+    }
+
+}
---
+++ member/src/main/java/com/wzg/train/member/service/PassengerService.java
@@ -0,0 +1,27 @@
+package com.wzg.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.wzg.train.common.utils.SnowUtil;
+import com.wzg.train.member.domain.Passenger;
+import com.wzg.train.member.mapper.PassengerMapper;
+import com.wzg.train.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    public void save(PassengerSaveReq req){
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+
+    }
+}
