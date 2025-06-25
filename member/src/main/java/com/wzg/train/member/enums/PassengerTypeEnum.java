f961c3b4f249320e361210d10279fab3547cfe9b
wzg <wzg7014@163.com>
2025-06-25 18:25:58 +0800

    7.2 增加乘车人类型枚举

---
+++ member/src/main/java/com/wzg/train/member/enums/PassengerTypeEnum.java
@@ -0,0 +1,41 @@
+package com.wzg.train.member.enums;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+
+
+
+@Getter
+public enum PassengerTypeEnum {
+
+    ADULT("1", "成人"),
+    CHILD("2", "儿童"),
+    STUDENT("3", "学生");
+
+    @Setter
+    private String code;
+    @Setter
+
+    private String desc;
+
+    PassengerTypeEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public static List<HashMap<String,String>> getEnumList() {
+        List<HashMap<String, String>> list = new ArrayList<>();
+        for (PassengerTypeEnum anEnum : EnumSet.allOf(PassengerTypeEnum.class)) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("code",anEnum.code);
+            map.put("desc",anEnum.desc);
+            list.add(map);
+        }
+        return list;
+    }
+}
