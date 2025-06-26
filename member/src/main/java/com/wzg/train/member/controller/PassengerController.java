b0efdeb3b17476ea6468c6294f58532ebbcfb228
wzg <wzg7014@163.com>
2025-06-26 16:30:34 +0800

    7.8 增加乘车人列表查询接口

--- member/src/main/java/com/wzg/train/member/controller/PassengerController.java
+++ member/src/main/java/com/wzg/train/member/controller/PassengerController.java
@@ -1,14 +1,16 @@
 package com.wzg.train.member.controller;
 
+import com.wzg.train.common.context.LoginMemberContext;
 import com.wzg.train.common.resp.CommonResp;
+import com.wzg.train.member.req.PassengerQueryReq;
 import com.wzg.train.member.req.PassengerSaveReq;
+import com.wzg.train.member.resp.PassengerQuaryResp;
 import com.wzg.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -22,5 +24,13 @@ public class PassengerController {
         passengerService.save(req);
         return new CommonResp<>();
     }
+
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQuaryResp>> queryList(@Valid PassengerQueryReq req) {
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQuaryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
 
 }
--- member/src/main/java/com/wzg/train/member/service/PassengerService.java
+++ member/src/main/java/com/wzg/train/member/service/PassengerService.java
@@ -2,13 +2,19 @@ package com.wzg.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.wzg.train.common.context.LoginMemberContext;
 import com.wzg.train.common.utils.SnowUtil;
 import com.wzg.train.member.domain.Passenger;
+import com.wzg.train.member.domain.PassengerExample;
 import com.wzg.train.member.mapper.PassengerMapper;
+import com.wzg.train.member.req.PassengerQueryReq;
 import com.wzg.train.member.req.PassengerSaveReq;
+import com.wzg.train.member.resp.PassengerQuaryResp;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -24,6 +30,16 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
-
     }
+
+    public List<PassengerQuaryResp> queryList(PassengerQueryReq req){
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if (ObjectUtil.isNotEmpty(req.getMemberId())){
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        List<Passenger> passengers = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengers, PassengerQuaryResp.class);
+    }
+
 }
