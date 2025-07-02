a41c289ee5a685e84ff3a8a895f3b725552947a4
wzg <wzg7014@163.com>
2025-07-02 17:20:50 +0800

    9.3 创建admin模块，所有代码都是从web模块复制

---
+++ admin/src/components/the-sider.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-layout-sider width="200" style="background: #fff">
+    <a-menu
+        mode="inline"
+        v-model:selectedKeys="selectedKeys"
+        v-model:openKeys="openKeys"
+        :style="{ height: '100%', borderRight: 0 }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <coffee-outlined /> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
+</template>
+
+<script>
+import {defineComponent, ref, watch} from 'vue';
+import router from "@/router";
+
+export default defineComponent({
+  name: "the-sider-view",
+  setup() {
+    const selectedKeys = ref([]);
+
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
+    return {
+      selectedKeys
+    }
+  },
+});
+
+</script>
+
+<style >
+
+
+</style>
---
+++ admin/src/views/main/welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>欢迎使用彰广12306售票系统</h1>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  setup(){
+    return{
+
+    };
+  },
+})
+</script>
+
+<style>
+</style>
