842705805cc1a983d51ab3386dbd861819957026
wzg <wzg7014@163.com>
2025-07-03 14:24:13 +0800

    9.9 增加火车车厢表，生成持久层代码及前后端代码

---
+++ business/src/main/java/com/wzg/train/business/req/TrainCarriageQueryReq.java
@@ -0,0 +1,13 @@
+package com.wzg.train.business.req;
+
+import com.wzg.train.common.req.PageReq;
+
+public class TrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("TrainCarriageQueryReq{");
+        sb.append('}');
+        return sb.toString();
+    }
+}
