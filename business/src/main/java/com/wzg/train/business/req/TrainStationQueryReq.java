e3f832b146560b825afd890095685e0030146296
wzg <wzg7014@163.com>
2025-07-04 11:06:28 +0800

    9.15 为火车车站页面增加车次查询条件

--- business/src/main/java/com/wzg/train/business/req/TrainStationQueryReq.java
+++ business/src/main/java/com/wzg/train/business/req/TrainStationQueryReq.java
@@ -4,10 +4,20 @@ import com.wzg.train.common.req.PageReq;
 
 public class TrainStationQueryReq extends PageReq {
 
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("TrainStationQueryReq{");
-        sb.append('}');
-        return sb.toString();
+        return "TrainStationQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
+                "} " + super.toString();
     }
 }
