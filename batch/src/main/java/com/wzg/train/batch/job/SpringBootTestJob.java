4fb5e2ca8d47555afa6b06905d5d9011e20b8b90
wzg <wzg7014@163.com>
2025-07-05 12:36:55 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/wzg/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/wzg/train/batch/job/SpringBootTestJob.java
@@ -1,15 +1,15 @@
-package com.wzg.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-
-    @Scheduled(cron = "0/5 * * * * ?")
-    private void tets(){
-        System.out.println("SpringBootTestJob TEST!");
-    }
-}
+//package com.wzg.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+//
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    private void tets(){
+//        System.out.println("SpringBootTestJob TEST!");
+//    }
+//}
--- batch/src/main/java/com/wzg/train/batch/job/TestJob.java
+++ batch/src/main/java/com/wzg/train/batch/job/TestJob.java
@@ -1,13 +1,20 @@
 package com.wzg.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext)  {
-        System.out.println("TestJob TEST!");
+        System.out.println("TestJob TEST开始!");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob TEST结束!");
     }
 }
