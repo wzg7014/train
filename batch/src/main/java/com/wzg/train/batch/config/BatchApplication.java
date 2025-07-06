7a78b238311edd3dfa8b609f1a62ab6cfa7b7699
wzg <wzg7014@163.com>
2025-07-06 20:36:42 +0800

    11.8 集成openfeign，实现服务间调用,给每个应用增加应用名，可以搭配注册中心使用

--- batch/src/main/java/com/wzg/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/wzg/train/batch/config/BatchApplication.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.wzg")
 @MapperScan("com.wzg.train.*.mapper")
+@EnableFeignClients("com.wzg.train.batch.feign")
 public class BatchApplication {
 
 
--- batch/src/main/java/com/wzg/train/batch/controller/TestController.java
+++ batch/src/main/java/com/wzg/train/batch/controller/TestController.java
@@ -1,13 +1,24 @@
 package com.wzg.train.batch.controller;
 
+import com.wzg.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello() {
+        String hello = businessFeign.hello();
+        LOG.info(hello);
         return  "Hello World! Batch!";
     }
 }
