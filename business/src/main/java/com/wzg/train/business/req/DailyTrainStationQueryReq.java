66c64ed2cd5fbc3b535dca052762d2881a597960
wzg <wzg7014@163.com>
2025-07-06 15:46:23 +0800

    11.4 完善每日车站页面，增加日期和车次查询条件

--- business/src/main/java/com/wzg/train/business/req/DailyTrainStationQueryReq.java
+++ business/src/main/java/com/wzg/train/business/req/DailyTrainStationQueryReq.java
@@ -1,13 +1,22 @@
 package com.wzg.train.business.req;
 
 import com.wzg.train.common.req.PageReq;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ToString
 public class DailyTrainStationQueryReq extends PageReq {
 
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("DailyTrainStationQueryReq{");
-        sb.append('}');
-        return sb.toString();
-    }
+    private String trainCode;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
 }
