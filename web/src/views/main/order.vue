8a546f842496e961de2ae2d4a59108cf7901b13f
wzg <wzg7014@163.com>
2025-07-08 10:46:13 +0800

    12.7 美化车次信息的显示

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>{{dailyTrainTicket}}</div>
+  <span class="order-train-main">{{dailyTrainTicket.date}}</span>&nbsp;
+  <span class="order-train-main">{{dailyTrainTicket.trainCode}}</span>次&nbsp;
+  <span class="order-train-main">{{dailyTrainTicket.start}}</span>站
+  <span class="order-train-main">({{dailyTrainTicket.startTime}})</span>&nbsp;
+  <span class="order-train-main">——</span>&nbsp;
+  <span class="order-train-main">{{dailyTrainTicket.end}}</span>站
+  <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
 </template>
 
 <script>
@@ -9,7 +15,7 @@ export default defineComponent({
   name: "order-view",
   setup(){
     const dailyTrainTicket = SessionStorage.get(SESSION_ORDER) || {}
-    console.log("下单车票信息"+dailyTrainTicket);
+    console.log("下单车票信息", dailyTrainTicket);
     return{
       dailyTrainTicket
     }
@@ -18,5 +24,8 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
+.order-train .order-train-main {
+  font-size: 18px;
+  font-weight: bold;
+}
 </style>
