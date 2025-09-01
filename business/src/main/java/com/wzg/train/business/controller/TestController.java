3516b8aa654accbbe78ec45ab5c04733dc691bff
wzg <wzg7014@163.com>
2025-09-01 19:43:34 +0800

    17.9 sentinel限流模式讲解

--- business/src/main/java/com/wzg/train/business/controller/TestController.java
+++ business/src/main/java/com/wzg/train/business/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.wzg.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @SentinelResource("hello")
     @GetMapping("/hello")
-    public String hello() {
-         return  "Hello World! Business!";
+    public String hello() throws InterruptedException {
+//        Thread.sleep(500);
+        return  "Hello World! Business!";
     }
 }
