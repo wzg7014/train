a032da28f30f696307a9a31f082e1c3c6b04a123
wzg <wzg7014@163.com>
2025-09-02 13:11:28 +0800

    18.7 购票页面，调用购票接口之前，显示验证码

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -50,7 +50,7 @@
     <a-modal v-model:visible="visible" title="请核对以下信息"
              style="top: 50px; width: 800px"
              ok-text="确认" cancel-text="取消"
-             @ok="handleOk">
+             @ok="showImageCodeModal">
       <div class="order-tickets">
         <a-row class="order-tickets-header" v-if="tickets.length > 0">
           <a-col :span="3">乘客</a-col>
@@ -90,12 +90,26 @@
           </div>
           <div style="color: #999999">提示：您可以选择{{tickets.length}}个座位</div>
         </div>
-        <br/>
-        最终购票：{{tickets}}
-        最终选座：{{chooseSeatObj}}
+<!--        <br/>-->
+<!--        最终购票：{{tickets}}-->
+<!--        最终选座：{{chooseSeatObj}}-->
       </div>
     </a-modal>
   </div>
+  <!--验证码-->
+  <a-modal v-model:visible="imageCodeModalVisible"
+           :title="null" :footer="null" :closable="false"
+           style="top: 50px; width: 400px">
+    <p style="text-align: center;font-weight: bold; font-size: 18px">使用验证码削弱瞬时高峰</p>
+    <p>
+      <a-input v-model:value="imageCode" placeholder="图片验证码">
+        <template #suffix>
+          <img v-show="!!imageCodeSrc" :src="imageCodeSrc" alt="验证码" v-on:click="loadImageCode()">
+        </template>
+      </a-input>
+    </p>
+    <a-button type="danger" block @click="handleOk">请输入验证码后开始购票</a-button>
+  </a-modal>
 </template>
 
 <script>
@@ -289,6 +303,11 @@ export default defineComponent({
     };
 
     const handleOk = () => {
+      if (Tool.isEmpty(imageCode.value)) {
+        notification.error({description: '验证码不能为空'});
+        return;
+      }
+
       console.log("选好的座位：", chooseSeatObj.value);
 
       // 设置每张票的座位
@@ -334,6 +353,25 @@ export default defineComponent({
       });
     }
 
+    /*验证码*/
+    const imageCodeModalVisible = ref();
+    const imageCodeToken = ref();
+    const imageCodeSrc = ref();
+    const imageCode = ref();
+
+    /*加载图形验证码*/
+    const loadImageCode = () => {
+      imageCodeToken.value = Tool.uuid(8);
+      imageCodeSrc.value = process.env.VUE_APP_SERVER + '/business/kaptcha/image-code/'+ imageCodeToken.value;
+      console.log("imageCodeSrc:",imageCodeSrc.value)
+    };
+
+    const showImageCodeModal = () =>{
+      loadImageCode();
+      imageCodeModalVisible.value = true;
+      console.log("点击确认了",imageCodeModalVisible.value)
+    };
+
     onMounted(() => {
       handleQueryPassenger()
     })
@@ -352,7 +390,13 @@ export default defineComponent({
       chooseSeatType,
       SEAT_COL_ARRAY,
       chooseSeatObj,
-      handleOk
+      handleOk,
+      imageCodeModalVisible,
+      imageCodeToken,
+      imageCodeSrc,
+      imageCode,
+      loadImageCode,
+      showImageCodeModal
     }
   }
 })
