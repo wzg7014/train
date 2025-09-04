a83da3d0ad47f958053c03af56d7f44fd6f36b4c
wzg <wzg7014@163.com>
2025-09-04 09:17:57 +0800

    22.5 增加查询座位销售详情接口

--- business/src/main/java/com/wzg/train/business/service/DailyTrainSeatService.java
+++ business/src/main/java/com/wzg/train/business/service/DailyTrainSeatService.java
@@ -9,6 +9,8 @@ import cn.hutool.core.util.StrUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.wzg.train.business.domain.*;
+import com.wzg.train.business.req.SeatSellReq;
+import com.wzg.train.business.resp.SeatSellResp;
 import com.wzg.train.common.resp.PageResp;
 import com.wzg.train.common.utils.SnowUtil;
 import com.wzg.train.business.mapper.DailyTrainSeatMapper;
@@ -143,5 +145,20 @@ public class DailyTrainSeatService {
                 .andTrainCodeEqualTo(trainCode)
                 .andCarriageIndexEqualTo(carriageIndex);
         return dailyTrainSeatMapper.selectByExample(dailyTrainSeatExample);
+    }
+
+    /**
+     * 查询某日某车次的所有座位
+     */
+    public List<SeatSellResp> querySeatSell(SeatSellReq req) {
+        Date date = req.getDate();
+        String trainCode = req.getTrainCode();
+        LOG.info("查询日期【{}】车次【{}】的座位销售信息", DateUtil.formatDate(date), trainCode);
+        DailyTrainSeatExample dailyTrainSeatExample = new DailyTrainSeatExample();
+        dailyTrainSeatExample.setOrderByClause("`carriage_index` asc, carriage_seat_index asc");
+        dailyTrainSeatExample.createCriteria()
+                .andDateEqualTo(date)
+                .andTrainCodeEqualTo(trainCode);
+        return BeanUtil.copyToList(dailyTrainSeatMapper.selectByExample(dailyTrainSeatExample), SeatSellResp.class);
     }
 }
